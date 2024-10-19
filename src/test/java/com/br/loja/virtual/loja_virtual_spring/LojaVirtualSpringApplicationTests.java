817cd7d1ca533276535f5f670dfb61ff1f6e6cb9
camila-soares <camilasoares1507@gmail.com>
2024-10-19 19:20:25 -0300

    commit inicial

---
+++ src/test/java/com/br/loja/virtual/loja_virtual_spring/LojaVirtualSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.loja.virtual.loja_virtual_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
