8edcf26c54b7af3f39535ce107699553d7d76339
camila-soares <camilasoares1507@gmail.com>
2024-11-04 01:10:29 -0300

    correcao das trigers da tabela pessoa

--- src/main/java/com/br/loja/virtual/loja_virtual_spring/security/JWTTokenAuthenticationService.java
+++ src/main/java/com/br/loja/virtual/loja_virtual_spring/security/JWTTokenAuthenticationService.java
@@ -32,6 +32,28 @@ public class JWTTokenAuthenticationService {
 
         response.addHeader(AUTHORIZATION, token);
 
+        liberacaoCors(response);
+
         response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
+    }
+
+    private void liberacaoCors(HttpServletResponse response) throws IOException {
+
+        if( response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        };
+
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+        }
+
+        if (response.getHeader("Access-Control-Request-Headers") == null) {
+            response.addHeader("Access-Control-Request-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "*");
+        }
+
     }
 }
